e44a51dc7d213053940ed01d4c3d08d770fb9ebc
Andres Camilo Urrego Restrepo <acur97.tk@gmail.com>
2020-07-09 19:37:02 -0500

    compresion imagenes

--- Assets/_Scripts/gps/ActivarSonido.cs
+++ Assets/_Scripts/gps/ActivarSonido.cs
@@ -9,13 +9,17 @@ public class ActivarSonido : MonoBehaviour
     private Collider otherActual;
     private Collider otherSalio;
 
+    private readonly string _sonido = "Sonido";
+    private readonly string _in = "in";
+    private readonly string _out = "out";
+
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Sonido"))
+        if (other.CompareTag(_sonido))
         {
             soure = other.GetComponent<AudioSource>();
             anim = other.GetComponent<Animator>();
-            anim.SetTrigger("in");
+            anim.SetTrigger(_in);
             soure.Play();
             otherActual = other;
         }
@@ -23,25 +27,25 @@ public class ActivarSonido : MonoBehaviour
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag("Sonido"))
+        if (other.CompareTag(_sonido))
         {
             otherSalio = other;
             if (otherSalio != otherActual)
             {
                 soure = otherSalio.GetComponent<AudioSource>();
                 anim = otherSalio.GetComponent<Animator>();
-                anim.ResetTrigger("in");
-                anim.ResetTrigger("out");
-                anim.SetTrigger("out");
+                anim.ResetTrigger(_in);
+                anim.ResetTrigger(_out);
+                anim.SetTrigger(_out);
                 anim = null;
                 soure.Pause();
                 soure = null;
             }
             else
             {
-                anim.ResetTrigger("in");
-                anim.ResetTrigger("out");
-                anim.SetTrigger("out");
+                anim.ResetTrigger(_in);
+                anim.ResetTrigger(_out);
+                anim.SetTrigger(_out);
                 anim = null;
                 soure.Pause();
                 soure = null;
--- Assets/_Scripts/gps/ObtenerGPS.cs
+++ Assets/_Scripts/gps/ObtenerGPS.cs
@@ -10,52 +10,48 @@ public class ObtenerGPS : MonoBehaviour
     public Transform cubeTest;
     public RectTransform cubeTestUI;
     public Transform cameraT;
-    public Vector3 offset;
+    public Vector3 offset = new Vector3(0, 0, 0);
 
-    private string testo;
-    //private bool primerPos;
-    //private double firstPosX;
-    //private double firstPosZ;
-    //private double calcPosX;
-    //private double calcPosZ;
-    private float nowPosX;
-    private float nowPosZ;
+    private string testo = "";
+    private float nowPosX = 0;
+    private float nowPosZ = 0;
     private bool modoDebug = true;
+    private int maxWait = 10;
+    private float compass = 0;
+
+    private readonly string _enter = "\n";
 
     IEnumerator Start()
     {
         Screen.sleepTimeout = SleepTimeout.NeverSleep;
 
         //probar si esta activado el servicio
-        if (!Input.location.isEnabledByUser)
-        {
-            if (modoDebug)
-            {
-                DebugText.text += "gps no activado";
-                Debug.Log("gps no activado");
-            }
-            //yield break;
-        }
+        //if (!Input.location.isEnabledByUser)
+        //{
+        //    if (modoDebug)
+        //    {
+        //        DebugText.text += "gps no activado";
+        //    }
+        //    //yield break;
+        //}
 
         //iniciar servicio
         Input.location.Start(1f, 0.1f);
         Input.compass.enabled = true;
 
         //espera a que inicie
-        int maxWait = 20;
         while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
         {
-            yield return new WaitForSeconds(1);
+            yield return new WaitForSecondsRealtime(1);
             maxWait--;
         }
 
-        //si no inicia en 20 segundos
+        //si no inicia en 10 segundos
         if (maxWait < 1)
         {
             if (modoDebug)
             {
-                DebugText.text += "\n" + "tiempo acabado";
-                Debug.Log("tiempo acabado");
+                DebugText.text += _enter + "tiempo acabado";
             }
             yield break;
         }
@@ -65,8 +61,7 @@ public class ObtenerGPS : MonoBehaviour
         {
             if (modoDebug)
             {
-                DebugText.text += "\n" + "imposible determinar ubicacion por gps";
-                Debug.Log("imposible determinar ubicacion por gps");
+                DebugText.text += _enter + "imposible determinar ubicacion por gps";
             }
             yield break;
         }
@@ -75,16 +70,9 @@ public class ObtenerGPS : MonoBehaviour
             //acceso aceptado
             if (modoDebug)
             {
-                Debug.Log("GPS aceptado");
-                DebugText.text += "\n" + "GPS aceptado";
-                Debug.Log("gps: " + Input.location.lastData.latitude + " " + Input.location.lastData.longitude + " " + Input.location.lastData.altitude + " " + Input.location.lastData.horizontalAccuracy + " " + Input.location.lastData.timestamp);
-                DebugText.text += "\n" + "gps: " + Input.location.lastData.latitude + " " + Input.location.lastData.longitude + " " + Input.location.lastData.altitude + " " + Input.location.lastData.horizontalAccuracy + " " + Input.location.lastData.timestamp;
+                DebugText.text += _enter + "GPS aceptado";
+                DebugText.text += _enter + "gps: " + Input.location.lastData.latitude + " " + Input.location.lastData.longitude + " " + Input.location.lastData.altitude + " " + Input.location.lastData.horizontalAccuracy + " " + Input.location.lastData.timestamp;
             }
-            /*if (!primerPos)
-            {
-                firstPosX = Input.location.lastData.latitude;
-                firstPosZ = Input.location.lastData.longitude;
-            }*/
         }
     }
 
@@ -95,31 +83,25 @@ public class ObtenerGPS : MonoBehaviour
         nowPosZ = Ldat.longitude;
         if (modoDebug)
         {
-            testo = "\n" + "Latitud: " + Ldat.latitude;
-            testo += "\n" + "Longitud: " + Ldat.longitude;
-            testo += "\n" + "Altitud: " + Ldat.altitude;
-            testo += "\n" + "Precision horizontal: " + Ldat.horizontalAccuracy;
-            testo += "\n" + "Precision vertical: " + Ldat.verticalAccuracy;
-            testo += "\n" + "Update´s: " + Time.time;
-            testo += "\n" + "\n" + "valor x: " + nowPosX;
-            testo += "\n" + "valor z: " + nowPosZ;
-            testo += "\n" + "\n" + "valor x(x10): " + (nowPosX * 10);
-            testo += "\n" + "valor z(x10): " + (nowPosZ * 10);
-            testo += "\n" + "\n" + "compass trueHeading: " + Input.compass.trueHeading;
+            testo = _enter + "Latitud: " + nowPosX;
+            testo += _enter + "Longitud: " + nowPosZ;
+            testo += _enter + "Altitud: " + Ldat.altitude;
+            testo += _enter + "Precision horizontal: " + Ldat.horizontalAccuracy;
+            testo += _enter + "Precision vertical: " + Ldat.verticalAccuracy;
+            //testo += "\n" + "Update´s: " + Time.time;
+            testo += _enter + _enter + "valor x(x10): " + (nowPosX * 10);
+            testo += _enter + "valor z(x10): " + (nowPosZ * 10);
+            testo += _enter + _enter + "compass trueHeading: " + Input.compass.trueHeading;
         }
-        //testo += "\n" + "Timestamp: " + Ldat.timestamp;
-        //testo += "\n" + "compass trueHeading(offset): " + (Input.compass.trueHeading + 270);
-        //testo += "\n" + "compass headingAccuracy: " + Input.compass.headingAccuracy;
-        //testo += "\n" + "compass magneticHeading: " + Input.compass.magneticHeading;
-        //testo += "\n" + "compass rawVector: " + Input.compass.rawVector;
-        //testo += "\n" + "compass timestamp: " + Input.compass.timestamp;
+
+        compass = Input.compass.trueHeading;
 
 #if !UNITY_EDITOR && UNITY_ANDROID
         cubeTest.localPosition = new Vector3((nowPosX * 10), 0, (nowPosZ * 10));
 
         ///Si el sonido esta al rever, agregarle +180 rotacion al objeto, primero revisar si con euler angles si funciona///
-        cubeTest.localEulerAngles = new Vector3(0, (Input.compass.trueHeading), 0);
-        cubeTestUI.localEulerAngles = new Vector3(0, 0, -(Input.compass.trueHeading));
+        cubeTest.localEulerAngles = new Vector3(0, compass, 0);
+        cubeTestUI.localEulerAngles = new Vector3(0, 0, -compass);
 #endif
         cameraT.position = new Vector3(cubeTest.position.x + offset.x, 1 + offset.y, cubeTest.position.z + offset.z);
 
