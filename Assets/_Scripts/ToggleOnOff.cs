fa83b3a69c116e6ce77ffb0d60f5799c17ca0638
Andres Camilo Urrego Restrepo <acur97.tk@gmail.com>
2022-03-21 13:56:39 -0500

    last cambios

---
+++ Assets/_Scripts/ToggleOnOff.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ToggleOnOff : MonoBehaviour
+{
+    public UnityEvent On;
+    public UnityEvent Off;
+
+    private void Awake()
+    {
+        Toggle(true);
+    }
+
+    public void Toggle (bool _on)
+    {
+        if (_on)
+        {
+            On.Invoke();
+        }
+        else
+        {
+            Off.Invoke();
+        }
+    }
+}
